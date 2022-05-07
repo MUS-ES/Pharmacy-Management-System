1983503dba69f634ddf95cddeed2dacba32be756
majd52 <majdsoubh53@gmail.com>
2022-05-07 17:52:37 +0300

    Make: sidebar, nav componenet

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,149 @@
+   <!-- Sidebar Section -->
+   <section id="sidebar" class="active">
+       <!-- Sidebar Brand -->
+       <a href="{{ asset('#') }}" class="active brand">
+           <span class="icon material-icons-outlined">local_hospital</span>
+           <h3 id="brand-txt">PMS</h3>
+       </a>
+       <!-- End Of Sidebar Brand -->
+
+       <!-- Sidebar Elements -->
+       <ul class="side-menu">
+
+           <li>
+               <a href="{{ asset('#') }}">
+                   <span class="icon material-icons-outlined">space_dashboard</span>
+                   <h3>Dashboard</h3>
+               </a>
+           </li>
+
+           <li>
+               <a href="{{ asset('#') }}">
+                   <span class="icon material-icons-outlined">list_alt</span>
+                   <h3>Invoices</h3>
+                   <span class="icon-right material-icons-outlined">expand_more</span>
+               </a>
+               <ul class="side-dropdown">
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">add</span>New Invoice</a></li>
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">edit</span>Manage Invoice</a>
+                   </li>
+               </ul>
+           </li>
+
+           <li>
+               <a href="{{ asset('#') }}">
+                   <span class="icon material-icons-outlined">personal_injury</span>
+                   <h3>Customers</h3>
+                   <span class="icon-right material-icons-outlined">expand_more</span>
+               </a>
+               <ul class="side-dropdown">
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">add</span>Add Customer</a>
+                   </li>
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">edit</span>Manage
+                           Customer</a>
+                   </li>
+               </ul>
+           </li>
+
+           <li>
+               <a href="{{ asset('#') }}">
+                   <span class="icon material-icons-outlined">medication</span>
+                   <h3>Medicines</h3>
+                   <span class="icon-right material-icons-outlined">expand_more</span>
+               </a>
+               <ul class="side-dropdown">
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">add</span>Add Medicine</a>
+                   </li>
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">edit</span>Manage
+                           Medicine</a>
+                   </li>
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">inventory_2</span>Manage
+                           Medicine Stock</a>
+                   </li>
+               </ul>
+           </li>
+
+           <li>
+               <a href="{{ asset('#') }}">
+                   <span class="icon material-icons-outlined">airport_shuttle</span>
+                   <h3>Suppliers</h3>
+                   <span class="icon-right material-icons-outlined">expand_more</span>
+               </a>
+               <ul class="side-dropdown">
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">add</span>Add Supplier</a>
+                   </li>
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">edit</span>Manage
+                           Supplier</a>
+                   </li>
+               </ul>
+           </li>
+
+           <li>
+               <a href="{{ asset('#') }}">
+                   <span class="icon material-icons-outlined">point_of_sale</span>
+                   <h3>Purchase</h3>
+                   <span class="icon-right material-icons-outlined">expand_more</span>
+               </a>
+               <ul class="side-dropdown">
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">add</span>Add Purchase</a>
+                   </li>
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">edit</span>Manage
+                           Purchase</a>
+                   </li>
+               </ul>
+           </li>
+
+           <li>
+               <a href="{{ asset('#') }}">
+                   <span class="icon material-icons-outlined">summarize</span>
+                   <h3>Reports</h3>
+                   <span class="icon-right material-icons-outlined">expand_more</span>
+               </a>
+               <ul class="side-dropdown">
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">summarize</span>Sales
+                           Report</a></li>
+                   <li><a href="{{ asset('#') }}"><span class="material-icons-outlined">summarize</span>Purchase
+                           Report</a></li>
+               </ul>
+           </li>
+
+           <li>
+               <a href="{{ asset('#') }}">
+                   <span class="icon material-icons-outlined">manage_search</span>
+                   <h3>Search</h3>
+                   <span class="icon-right material-icons-outlined">expand_more</span>
+               </a>
+               <ul class="side-dropdown">
+                   <li><a href="{{ asset('#') }}"><span
+                               class="icon material-icons-outlined">list_alt</span>Invoice</a>
+                   </li>
+                   <li><a href="{{ asset('#') }}"><span
+                               class="icon material-icons-outlined">personal_injury</span>Customer</a></li>
+                   <li><a href="{{ asset('#') }}"><span
+                               class="icon material-icons-outlined">medication</span>Medicine</a></li>
+                   <li><a href="{{ asset('#') }}"><span
+                               class="icon material-icons-outlined">airport_shuttle</span>Supplier</a></li>
+                   <li><a href="{{ asset('#') }}"><span
+                               class="icon material-icons-outlined">point_of_sale</span>Purchase</a></li>
+               </ul>
+           </li>
+
+           <li>
+               <a href="{{ asset('#') }}">
+                   <span class="icon material-icons-outlined">people_alt</span>
+                   <h3>About Us</h3>
+               </a>
+           </li>
+
+           <li>
+               <a href="{{ route('logout') }}">
+                   <span class="icon material-icons-outlined">logout</span>
+                   <h3>Sign Out</h3>
+               </a>
+           </li>
+
+       </ul>
+       <!-- End Of Sidebar Elements -->
+
+   </section>
+   <!-- End Of Sidebar -->
